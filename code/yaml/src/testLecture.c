077374d4acd672c553da6424446a644a00be7ea9
Kagari <aymeric.adenot27@orange.fr>
2020-11-07 19:55:56 +0000

    parser fonctionnel

--- code/yaml/src/testLecture.c
+++ code/yaml/src/testLecture.c
@@ -2,46 +2,67 @@
 #include <string.h>
 #include <yaml.h>
 
+//ma fonction pour voir si deux string sont egal 
+int static mystrcmp(const char* s1, const char* s2){
+    int i = 0;
+    do
+    {
+        if (s1[i] != s2[i])
+            return 1;
+        i++;
+    }
+    while (s1[i-1] != 0);
+    return 0;
+}
 int main(void) {
+    //ouverture du fichier
     FILE* fh = fopen("FR_fr.yaml", "r");
+
+    //on vérifie si le fichier à bien était ouvert
+    if (fh == NULL)
+      fputs("Erreur ouvertur yaml!\n", stderr);
+
+    //initialisation du parser et token
     yaml_parser_t parser;
     yaml_token_t token;
    
-
+    //erreur si l'initialisation échoue
     if (!yaml_parser_initialize(&parser))
-        fputs("Failed to initialize parser!\n", stderr);
-    if (fh == NULL)
-        fputs("Failed to open file!\n", stderr);
+        fputs("Erreur initialisation parser!\n", stderr);
+  
+    //on place le paser au début du fichier à lire
     yaml_parser_set_input_file(&parser, fh);
 
-
-    /* As this is an example, I'll just use:
-     *  state = 0 = expect key
-     *  state = 1 = expect value
-     */
-    int state = 0;
-    char** datap;
     char* tk;
-
+    //on boucle tout le long du fichier
     do {
         yaml_parser_scan(&parser, &token);
         switch(token.type) {
-            case YAML_KEY_TOKEN:     state = 0; break;
-            case YAML_VALUE_TOKEN:   state = 1; break;
-            case YAML_SCALAR_TOKEN:
-                tk = token.data.scalar.value;
-                if (state == 0) {
-                    printf("test %s ", tk);
-                }
-                break;
-           default: break;
+            /*il existe d'autre méthode mais dans autre cas seul 
+            le YAML_SCALAR_TOKEN nous intéresse*/
+                case YAML_SCALAR_TOKEN: 
+                //on récupére chaques éléments du fichier 
+                tk=token.data.scalar.value;
+
+                /*le premier élèment récupérer et le nom du dialecte
+                dans autre cas il nouos servira plus donc on traite ce cas*/
+                    if(mystrcmp(tk,"FR_fr")==0){
+                        puts("premier élèment");
+                    }else{
+                        //on s'occupe ensuite des éléèments qui nous interresse 
+                        printf("%s ", tk);
+                    }
+                 break;
+           default:break;
            }
        if (token.type != YAML_STREAM_END_TOKEN)
            yaml_token_delete(&token);
    } while (token.type != YAML_STREAM_END_TOKEN);
-    puts("\nfin");
+   puts("");
+   //on libère notre token et notre parser
    yaml_token_delete(&token);
    yaml_parser_delete(&parser);
+   //on en a fini avec le fichier
    fclose(fh);
      
 return EXIT_SUCCESS;
