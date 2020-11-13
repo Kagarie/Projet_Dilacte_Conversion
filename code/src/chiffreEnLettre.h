94231346a41c3a9f39fc4423eed056810926cd32
Kagari <aymeric.adenot27@orange.fr>
2020-11-13 14:29:22 +0000

    création d'une classe et ses .h , .c associé

--- code/src/chiffreEnLettre.h
+++ code/src/chiffreEnLettre.h
@@ -1,12 +1,17 @@
-typedef  struct TabChiffreLettre TabChiffreLettre;
-struct TabChiffreLettre{
-    int index;
-    struct ChiffreLettre[];
-};
+typedef struct _ChiffreLettre ChiffreLettre;
 
-typedef struct ChiffreLettre ChiffreLettre;
-struct ChiffreLettre
-{
-    int nombre;
-    char nombreEnLettre;
-};
+extern void *(*chiffreEnLettre_malloc)(size_t size);
+
+extern void *(*chiffreEnLettre_realloc)(void *ptr, size_t size);
+
+extern void (*chiffreEnLettre_free)(void *ptr);
+
+extern unsigned int chiffreEnLettre_get_nombre(const ChiffreEnLettre *chiffreEnLettre);
+
+extern char chiffreEnLettre_get_mot(const ChiffreEnLettre *chiffreEnLettre);
+
+extern static struct ChiffreEnLettre *_fill(ChiffreEnLettre *chiffreEnLettre, const unsigned nombre, const char mot);
+
+extern static ChiffreEnLettre *_create(const unsigned int nombre, const char mot);
+
+extern void chiffreEnLettre_destroy(ChiffreEnLettre *chiffreEnLettre);
