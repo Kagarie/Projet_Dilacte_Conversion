d3f6bbeeba1190da6564c1a0082ba11c4132520b
Kagari <aymeric.adenot27@orange.fr>
2020-11-25 19:29:39 +0100

    correction cmakelists (a finir) et ajout de fichier cmake.in

---
+++ code/src/cmake-build-debug/chiffreEnLettre/CMakeFiles/chiffreEnLettre.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chiffreEnLettre.dir/chiffreEnLettre.c.o"
+  "libchiffreEnLettre.pdb"
+  "libchiffreEnLettre.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/chiffreEnLettre.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ code/src/cmake-build-debug/chiffreEnLettre/CMakeFiles/test-get.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test-get.dir/test-get.c.o"
+  "test-get"
+  "test-get.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/test-get.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
