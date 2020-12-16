98b90ed3518bc5f7aa2ecd21770f4f456b69fa29
Kagari <aymeric.adenot27@orange.fr>
2020-12-16 17:52:45 +0100

    delete code/

--- code/build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/aymeric/code/C/Projet_Dilacte_Conversion/code/src
-# Build directory: /home/aymeric/code/C/Projet_Dilacte_Conversion/code/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("chiffreEnLettre")
-subdirs("utils")
-subdirs("array")
